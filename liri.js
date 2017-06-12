8e3472c3292768727d7f914679f603b90b408ddc
funduval <fundaduval@gmail.com>
2017-06-11 23:54:05 -0400

    OMDB API data has been parsed but I need to assign movie parameters to a variable

--- liri.js
+++ liri.js
@@ -4,72 +4,70 @@
      var auth = keys.twitterKeys;
      var action = process.argv[2];
      var movie = process.argv[3];
-   
-    
 
-     switch (action) {
-
-case "my-tweets":
-tweet();
-break;
-
-case "movie-this":
-movie();
-break;
-
-}
-
-//need variables for ajax urls and keys
 
 var movieUrl = 'http://www.omdbapi.com/?i=tt3896198&apikey=40e9cece'
 
 var posterUrl = 'http://img.omdbapi.com/?i=tt3896198&h=600&apikey=40e9cece'
+   
 
-//they will probably have to be passed into the functions
+     switch (action) {
 
-//Function Junction
+case "my-tweets":
 
-function tweet(auth){
-	
-/* This will show your last 20 tweets and when they were created at in your terminal/bash window.*/
-
-			var Twitter = require('twitter');
+var Twitter = require('twitter');
 			 
-			var client = new Twitter({
+var client = new Twitter({
 			  consumer_key: auth.consumer_key,
 			  consumer_secret: auth.consumer_secret,
-			  access_token_key:  auth.access_token_key,
+			  access_token_key: auth.access_token_key,
 			  access_token_secret: auth.token
 			});
  
-			var params = {screen_name: 'nodejs'};
+var params = {screen_name: 'nodejs'};
 			client.get('statuses/user_timeline', params, function(error, tweets, response) {
 			  if (!error) {
 			    console.log(tweets);
 			  }
 			});
+break;
 
-			var request = require('request');
-			request('http://www.google.com', function (error, response, body) {
-			  console.log('error:', error); // Print the error if one occurred 
-			  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-			  console.log('body:', body); // Print the HTML for the Google homepage. 
+case "movie-this":
+
+var request = require('request');
+
+			request(movieUrl, function (error, response, body) {
+					
+			  body = body.split('"');
+
+							  if (error) {
+							    console.log('error:', error);
+							  }
+
+			  // console.log(body);
+
+			  console.log(body[body.indexOf("Title") + 2] + ", " + body[body.indexOf("Year") + 2]); 
+			  console.log("IMDb Rating: " + body[body.indexOf("imdbRating") + 2]); 
+			  console.log("Rotten Tomatoes: " + body[body.indexOf("Rotten Tomatoes") + 4]);
+			  console.log("Actors: " + body[body.indexOf("Actors") + 2]);
+			  console.log("Language: " + body[body.indexOf("Language") + 2]);
+			  console.log("Country of Release: " + body[body.indexOf("Country") + 2]);
+			  console.log("Plot: " + body[body.indexOf("Plot") + 2]);
+			  console.log("Rotten Tomatoes URL: ") 
+			
+			  // console.log('statusCode:', response && response.statusCode); 
+
 			});
 
-}
-
-function movie(movieUrl){
-
-				$.ajax ({
-			url: movieUrl,
-			Method: “GET”
-			}).done(function(response) {
-
-			console.log(response);
-
-		});
+break;
 
 }
+
+       
+
+
+
+
 
 
 
